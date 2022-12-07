0e14f86bc5128f384226d644fc5612530c329258
Doonywoony <javismcarr@gmail.com>
2022-12-07 17:46:26 -0500

    added experience/need to modify styling

--- react-portfolio/src/App.jsx
+++ react-portfolio/src/App.jsx
@@ -1,12 +1,13 @@
 import React from 'react'
-import Header from "./components/header/Header";
+import Header from "./components/header/Header"
 import About from './components/about/About'
-
+import Experience from './components/experience/Experience'
 const App = () => {
   return (
     <>
       <Header/>
       <About/>
+      <Experience/>
       
     </>
   )
